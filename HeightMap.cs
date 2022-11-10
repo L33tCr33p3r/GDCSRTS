7cc286fd125832f26bd3158770be2d8ce47d6c5e
cheetahspots01 <112513827+752986@users.noreply.github.com>
2022-11-09 19:25:49 -0700

    First working 3d terrain

--- HeightMap.cs
+++ HeightMap.cs
@@ -1,18 +1,28 @@
 using Godot;
 using System;
 
-public class HeightMap
+public partial class HeightMap : Node
 {
+	[Export]
+	public int size = 100;
+
 	public float[,] Ground { get; protected set; }
 	public float[,] Water { get; protected set; }
-	public HeightMap(int size, int seed, float scale = 1.0f)
+
+	public override void _Ready()
+	{
+		generateData(0);
+		generateMesh();
+	}
+
+	private void generateData(int seed, float scale = 1.0f)
 	{
 		Ground = new float[size, size];
 		Water = new float[size, size];
 
 		FastNoiseLite noise = new();
 		noise.Seed = seed;
-
+		
 		for (int x = 0; x < size; x++)
 		{
 			for (int y = 0; y < size; y++)
@@ -20,5 +30,25 @@ public class HeightMap
 				Ground[x, y] = noise.GetNoise2d(x * scale, y * scale);
 			}
 		}
+	}
+
+	private void generateMesh() 
+	{
+		var st = new SurfaceTool();
+		st.Begin(Mesh.PrimitiveType.Triangles); // TODO: use tristrips instead
+		st.SetColor(new Color(0.8f, 0.8f, 0.8f));
+		for (int x = 0; x < size; x++)
+		{
+			for (int y = 0; y < size; y++)
+			{
+				st.AddVertex(new Vector3(x * 0.05f, Ground[x, y], y * 0.05f + 0.05f));
+				st.AddVertex(new Vector3(x * 0.05f, Ground[x, y], y * 0.05f));
+				st.AddVertex(new Vector3(x * 0.05f + 0.05f, Ground[x, y], y * 0.05f));
+			}
+		}
+
+		st.GenerateNormals();
+
+		GetNode<MeshInstance3D>("MeshInstance3D").Mesh = st.Commit();
 	}
 }
