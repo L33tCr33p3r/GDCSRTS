ae211466154b97248ee6ddced9d3e5a8285980ca
Dec <112513827+752986@users.noreply.github.com>
2022-11-30 14:49:48 -0700

    Update FlowField.cs

--- Script/FlowField.cs
+++ Script/FlowField.cs
@@ -1,5 +1,25 @@
 ﻿// Contains pathfinding Flowfields and their related methods 
 internal class FlowField
 {
-	// Stuff goes here
+	public Vector2[,]? Field { get; private set; }
+	public Vector2i Target { get; init; }
+	public HeightMap Terrain { get; init; }
+
+	private int[,] _distanceField { get; set; }
+	private int[,] _steepnessField { get; set; }
+
+	public FlowField(HeightMap heightMap) 
+	{
+		Terrain = heightMap;
+	}
+	
+	public void GenerateFlowField(float steepnessWeight = 1)
+	{
+
+	}
+
+	public void GenerateDistanceField()
+	{
+
+	}
 }
