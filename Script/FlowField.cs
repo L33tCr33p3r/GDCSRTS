47268dde99d43d8674b97e9a3d2c7277db3dffc3
Coryvin Peterson <coryvin@thebandit.org>
2022-12-07 13:30:07 -0700

    Pathfinding Rework in progress

--- Script/FlowField.cs
+++ Script/FlowField.cs
@@ -3,19 +3,14 @@ internal class FlowField
 {
 	// Properties (ALL OF THESE ARE PUBLIC)
 	public float?[,] DistanceField { get; private set; }
-	private HeightMap Terrain { get; init; }
-	public Vector2i Target { get; init; }
-	public float MaxSlope { get; init; }
+	public List<Vector2i> NodePath { get; private set; } = new();
+	public List<Vector2> VectorPath { get; private set; } = new();
 
 	// Constructor
-	public FlowField(HeightMap heightMap, Vector2i target, float maxSlope)
+	public FlowField(HeightMap heightMap, Vector2 target, Vector2 startPosition, float maxSlope)
 	{
-		Terrain = heightMap;
-		Target = target;
-		MaxSlope = maxSlope;
-
 		// instantiate and fill the array with nulls:
-		DistanceField = new float?[Terrain.Ground.GetLength(0), Terrain.Ground.GetLength(1)];
+		DistanceField = new float?[heightMap.Ground!.GetLength(0), heightMap.Ground.GetLength(1)];
 		for (int i = 0; i < DistanceField.GetLength(0); i++)
 		{
 			for (int j = 0; j < DistanceField.GetLength(1); j++)
@@ -24,10 +19,12 @@ internal class FlowField
 			}
 		}
 
-		GenerateDistanceField();
+		GenerateDistanceField(heightMap, (Vector2i) target, maxSlope);
+		GenerateNodePath(heightMap, (Vector2i) startPosition, maxSlope);
+		GenerateVectorPath();
 	}
 
-	public Vector2i Sample(Vector2i samplePoint) // TODO: fix random spots of null
+	public Vector2i Sample(HeightMap heightMap, Vector2i samplePoint, float maxSlope) // TODO: fix random spots of null
 	{
 		// TODO: make this interpolate the input and output vectors
 		int i = samplePoint.x;
@@ -35,9 +32,9 @@ internal class FlowField
 
 		float? minCost = null;
 		var minPoint = new Vector2i(0, 0);
-		foreach (int di in new int[] {-1, 0, 1})
+		foreach (int di in new int[] { -1, 0, 1 })
 		{
-			foreach (int dj in new int[] {-1, 0, 1})
+			foreach (int dj in new int[] { -1, 0, 1 })
 			{
 				int iprime = i + di;
 				int jprime = j + dj;
@@ -49,8 +46,8 @@ internal class FlowField
 				{
 					float sampled = (float)DistanceField[iprime, jprime]!;
 					float distance = Math.Abs(di) != Math.Abs(dj) ? 1 : Mathf.Sqrt(2);
-					float slope = (Terrain.Ground[iprime, jprime] - Terrain.Ground[i, j]) / distance;
-					if ((minCost == null || sampled < minCost) && slope < MaxSlope)
+					float slope = (heightMap.Ground![iprime, jprime] - heightMap.Ground[i, j]) / distance;
+					if ((minCost == null || sampled < minCost) && slope < maxSlope)
 					{
 						minCost = sampled;
 						minPoint = new Vector2i(di, dj);
@@ -93,7 +90,10 @@ internal class FlowField
 	// 	return result.Normalized();
 	// }
 
-	private void GenerateDistanceField()
+	/// <summary>
+	/// Generates the distance field that constistutes the actual flowfield
+	/// </summary>
+	private void GenerateDistanceField(HeightMap Terrain, Vector2i Target, float MaxSlope)
 	{
 		DistanceField[Target.x, Target.y] = 0;
 		bool fieldChanged;
@@ -123,7 +123,7 @@ internal class FlowField
 								if (DistanceField[iprime, jprime] != null)
 								{
 									float distance = di == 0 || dj == 0 ? 1 : Mathf.Sqrt(2); // gets the actual distance to the square rather than manhattan distance
-									float slope = (Terrain.Ground[i, j] - Terrain.Ground[iprime, jprime]) / distance;
+									float slope = (Terrain.Ground![i, j] - Terrain.Ground[iprime, jprime]) / distance;
 									float thisCost = (float)DistanceField[iprime, jprime]! + distance + slope;
 									if ((minCost == null || thisCost < minCost) && slope < MaxSlope)
 									{
@@ -140,5 +140,27 @@ internal class FlowField
 			}
 		} 
 		while (fieldChanged);
+	}
+
+	/// <summary>
+	/// Generates the node path from the unit's initial position to the path's end
+	/// </summary>
+	private void GenerateNodePath(HeightMap heightMap, Vector2i startPos, float maxSlope)
+	{
+		var currentNode = startPos;
+		NodePath.Add(currentNode);
+		while (DistanceField[currentNode.x, currentNode.y] != 0)
+		{
+			currentNode += Sample(heightMap, startPos, maxSlope);
+			NodePath.Add(currentNode);
+		}
+	}
+
+	/// <summary>
+	/// Generates the final path to be followed by the unit from the DistanceField and NodePath
+	/// </summary>
+	private void GenerateVectorPath()
+	{
+
 	}
 }
